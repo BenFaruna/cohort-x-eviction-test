33c65d6b266d8ea2d2605a9ea1e2386e3d0d90c2
Benjamin Faruna <benjaminfaruna@gmail.com>
2024-02-26 20:39:07 +0100

    made some changes to interaction.ts

--- scripts/uniswapInteraction.ts
+++ scripts/uniswapInteraction.ts
@@ -4,30 +4,30 @@ import { parseEther, parseUnits } from "ethers";
 const helpers = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 
 const main = async () => {
-    const USDCAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
-    const DAIAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
-    const wethAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+    const USDCAddress = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
+    const DAIAddress = "0x6b175474e89094c44da98b954eedeac495271d0f";
+    const WETHAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 
     const UNIRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
 
-    const USDCHolder = "0xf584f8728b874a6a5c7a8d4d387c9aae9172d621";
+    const ImpersonatedUserAddress = "0xf584F8728B874a6a5c7A8d4d387C9aae9172D621";
 
-    await helpers.impersonateAccount(USDCHolder);
-    const impersonatedSigner = await ethers.getSigner(USDCHolder);
+    await helpers.impersonateAccount(ImpersonatedUserAddress);
+    const impersonatedSigner = await ethers.getSigner(ImpersonatedUserAddress);
 
     const USDC = await ethers.getContractAt("IERC20", USDCAddress);
     const DAI = await ethers.getContractAt("IERC20", DAIAddress);
-    const WETH = await ethers.getContractAt("IERC20", wethAddress);
+    const WETH = await ethers.getContractAt("IERC20", WETHAddress);
 
     const ROUTER = await ethers.getContractAt("IUniswap", UNIRouter);
 
-    const approveUSDCTx = await USDC.connect(impersonatedSigner).approve(UNIRouter, parseUnits("220", 18));
+    const approveUSDCTx = await USDC.connect(impersonatedSigner).approve(UNIRouter, parseUnits("500", 6));
     approveUSDCTx.wait()
 
-    const approveDAITx = await DAI.connect(impersonatedSigner).approve(UNIRouter, parseUnits("200", 18));
+    const approveDAITx = await DAI.connect(impersonatedSigner).approve(UNIRouter, parseUnits("500", 6));
     approveDAITx.wait()
 
-    const ethBal = await impersonatedSigner.provider.getBalance(USDCHolder);
+    const ethBal = await impersonatedSigner.provider.getBalance(ImpersonatedUserAddress);
     const userWethBal = await WETH.balanceOf(impersonatedSigner.address);
 
     const usdcBal = await USDC.balanceOf(impersonatedSigner.address);
@@ -39,26 +39,32 @@ const main = async () => {
     console.log("-------------------------------Adding liquidity----------------------------------")
 
     const liquidityTx = await ROUTER.connect(impersonatedSigner)
-        .addLiquidityETH(USDCAddress, parseUnits("20", 6), parseUnits("10", 6),
-        parseEther("10"), USDCHolder, (Date.now() + (60*15)),
-        {value: parseEther("20")});
+        .addLiquidityETH(USDCAddress, 20_000_000_000n, 100_000n,
+        parseEther("2"), ImpersonatedUserAddress, (Date.now() + (60*15)),
+        {value: parseEther("2")});
     liquidityTx.wait()
 
-    console.log("WETH Balance after adding liquidity:", ethers.formatUnits(await WETH.balanceOf(USDCHolder), 18));
-    console.log("ETH Balance after adding liquidity:", ethers.formatUnits(await impersonatedSigner.provider.getBalance(USDCHolder), 18));
-    console.log("USDC Balance after adding liquidity:", ethers.formatUnits(await USDC.balanceOf(USDCHolder), 6));
+    console.log("WETH Balance after adding liquidity:", ethers.formatUnits(await WETH.balanceOf(ImpersonatedUserAddress), 18));
+    console.log("ETH Balance after adding liquidity:", ethers.formatUnits(await impersonatedSigner.provider.getBalance(ImpersonatedUserAddress), 18));
+    console.log("USDC Balance after adding liquidity:", ethers.formatUnits(await USDC.balanceOf(ImpersonatedUserAddress), 6));
 
 
     console.log("-------------------------------Adding liquidity for ERC20-ERC20 pool----------------------------------")
 
+    const approveUSDCTwoTx = await USDC.connect(impersonatedSigner).approve(UNIRouter, parseUnits("500", 6));
+    approveUSDCTwoTx.wait()
+
+    const approveDAITwoTx = await DAI.connect(impersonatedSigner).approve(UNIRouter, parseUnits("500", 6));
+    approveDAITwoTx.wait()
+
+
     const addLiquidityERC20Tx = await ROUTER.connect(impersonatedSigner)
-        .addLiquidity(USDCAddress, DAIAddress, parseUnits("200", 6), parseUnits("200", 6),
-        parseUnits("100", 6), parseUnits("100", 6), USDCHolder, (Date.now() + (60*15)));
+        .addLiquidity(USDCAddress, DAIAddress, 10_000n, 10_000n,
+        5_000n, 5_000n, ImpersonatedUserAddress, (Date.now() + (60*15)));
     addLiquidityERC20Tx.wait()
 
-    console.log("WETH Balance after removing liquidity:", ethers.formatUnits(await WETH.balanceOf(USDCHolder), 18));
-    console.log("ETH Balance after removing liquidity:", ethers.formatUnits(await impersonatedSigner.provider.getBalance(USDCHolder), 18));
-    console.log("USDC Balance after removing liquidity:", ethers.formatUnits(await USDC.balanceOf(USDCHolder), 6));
+    console.log("DAI Balance after adding liquidity:", ethers.formatUnits(await DAI.balanceOf(ImpersonatedUserAddress), 18));
+    console.log("USDC Balance after removing liquidity:", ethers.formatUnits(await USDC.balanceOf(ImpersonatedUserAddress), 6));
 }
 
 main().catch((error) => {
